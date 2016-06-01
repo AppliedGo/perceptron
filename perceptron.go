bd2b2b2ec8d46300c1e94ca4c02ad51327dcc2e5
Christoph Berger <christophberger@users.noreply.github.com>
2016-06-01 08:24:33 +0200

    Initial commit.

---
+++ perceptron.go
@@ -0,0 +1,26 @@
+/*
+<!--
+Copyright (c) 2016 Christoph Berger. Some rights reserved.
+Use of this text is governed by a Creative Commons Attribution Non-Commercial
+Share-Alike License that can be found in the LICENSE.txt file.
+
+The source code contained in this file may import third-party source code
+whose licenses are provided in the respective license files.
+-->
+
++++
+title = ""
+description = ""
+author = "Christoph Berger"
+email = "chris@appliedgo.com"
+date = "2016-00-00"
+publishdate = "2016-00-00"
+domains = [""]
+tags = ["", "", ""]
+categories = ["Tutorial"]
++++
+
+### Summary goes here 
+
+<!--more-->
+
